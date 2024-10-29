944b100817e37d7ddcb084b38ccce0cb4355c5af
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 10:40:05 +0000

    removed C

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let urlDatabase = {};
 let shortUrlIndex=1; 
 app.post('/api/shorturl',(req,res)=>{
   let url = req.body.url;
-  if(!url)c{
+  if(!url){
     return res.json({error:'invalid url'});
   }
   urlDatabase[shortUrlIndex] = url;
